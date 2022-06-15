79d534ffdbe64977ddc7c721256d5e143346cbfd
polthemarketer <psocarde@gmail.com>
2022-06-15 22:36:39 +0300

    Structure For Project's Page

--- pages/projects/[project].jsx
+++ pages/projects/[project].jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, GridItem, Box } from '@chakra-ui/react'
+import { Grid, GridItem, Box, Text } from '@chakra-ui/react'
 
 const Project = () => {
   return (
@@ -9,26 +9,59 @@ const Project = () => {
      <Grid
   templateRows='repeat(2, 1fr)'
   templateColumns='repeat(5, 1fr)'
-  gap={2} h='calc(80vh)' maxW={'100%'} mt={20} mb={4} mx={2}
+  gap={2} 
+  h='calc(80vh)' 
+  maxW={'100%'} 
+  mt={20} 
+  mb={4} 
+  mx={2} 
+  color='white'
 >
-  <GridItem rowSpan={2} colSpan={1} bgGradient='linear(to-r, #141E30, #243B55)' borderWidth='1px' borderRadius='lg'>
+    <GridItem colSpan={['5', '5', '4', '4', '4']} 
+    bgGradient='linear(to-r, #141E30, #243B55)' 
+    borderWidth='1px' 
+    borderRadius='lg' 
+    p={4}>
 
-  </GridItem>
+        <Text>Banner</Text>
+
+    </GridItem>
+
+    <GridItem rowSpan={['1', '2', '2', '2', '2']} 
+    colSpan={['5', '5', '1', '1', '1']} 
+    bgGradient='linear(to-r, #141E30, #243B55)'
+    borderWidth='1px' 
+    borderRadius='lg' 
+    p={4}
+    >
+
+        <Text>Transactions</Text>
+
+    </GridItem>
+
+  
+
+    <GridItem 
+    colSpan={['5', '5', '2', '2', '2']}  
+    bgGradient='linear(to-r, #141E30, #243B55)' 
+    borderWidth='1px' 
+    borderRadius='lg' 
+    p={4}>
+
+        <Text>Details</Text>
+
+    </GridItem>
 
 
-  <GridItem colSpan={2} bgGradient='linear(to-r, #141E30, #243B55)' borderWidth='1px' borderRadius='lg'>
+    <GridItem colSpan={['5', '5', '2', '2', '2']}  
+    bgGradient='linear(to-r, #141E30, #243B55)' 
+    borderWidth='1px' 
+    borderRadius='lg' 
+    p={4}>
 
-  </GridItem>
+        <Text>Mint</Text>
 
-
-  <GridItem colSpan={2} bgGradient='linear(to-r, #141E30, #243B55)' borderWidth='1px' borderRadius='lg'>
-
-  </GridItem>
-
-
-  <GridItem colSpan={4} bgGradient='linear(to-r, #141E30, #243B55)' borderWidth='1px' borderRadius='lg'>
-      
-  </GridItem>
+    </GridItem>
 
 
 </Grid>
