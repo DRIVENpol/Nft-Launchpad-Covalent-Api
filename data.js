a3632cebb6457bf21eac4e537e0cb8d835181265
polthemarketer <psocarde@gmail.com>
2022-06-20 23:36:26 +0300

    Update data.js

--- data.js
+++ data.js
@@ -26,14 +26,5 @@ export const projects =  [
          "twitter": "https://twitter.com/CyberKnightsNFT",
          "address": "0x8a33e477F73D22960D850Ff61FD8C58b3B2E21b3"
        },
-       {
-        "name": "Cyber Knights",
-         "type": "A collection of 10,000 unique Cyber Knights NFTs to protect the Crypto Realm! A Cyber Knight’s sole mission is to protect its owner from the evil forces that roam the DeFi space.As a core component of the DRIVENecosystem, these sentries are much more than art that sits dormant in your wallet; they are your gatekeeper, your protector, and your companion.",
-         "link": "https://lh3.googleusercontent.com/G5V8Tny1jONBKHZfsAEiOVUYaKMUMgQuo-VwfHBa8_AAgsmAuWdz7lPq6UUmK5dCfilaaNBIOe5l1UoqiHAOyDoD5lAKZpxZtmtOZQ=h600",
-         "website": "https://opensea.io/collection/cyber-knights-drivenecosystem",
-         "discord": "https://t.co/dohqCh54hj",
-         "twitter": "https://twitter.com/CyberKnightsNFT",
-         "address": "0x8a33e477F73D22960D850Ff61FD8C58b3B2E21b3"
-       },
          
        ]
